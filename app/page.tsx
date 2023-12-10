daea97e0b7e5d4dfbd27351121ad3882494f9591
senthil <131505586+senthil-developer@users.noreply.github.com>
2023-12-10 16:32:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import ComparesSlider from '@/components/ComparesSlider'
 import Link from 'next/link'
-import { SignInButton } from "@clerk/nextjs";
+import { SignIn} from "@clerk/nextjs";
 
 export default function Home() {
   return (
@@ -10,9 +10,9 @@ export default function Home() {
         <p className='text-gray-500 text-lg text-center'>
           took a picture of a room and instantly redesign it in 8+ different themes options.Join thousand of happy customers and remodel your dream house today.
         </p>
-         <SignInButton afterSignInUrl='/room' afterSignUpUrl='/room'>
+         <SignIn afterSignInUrl='/room' afterSignUpUrl='/room'>
             <button className='bg-blue-500 rounded-sm hover:opacity-90 font-semibold py-3 px-5 '>Redesign your room</button>
-         </SignInButton>
+         </SignIn>
       </div>
       {/* compare slider component */}
       <div>
